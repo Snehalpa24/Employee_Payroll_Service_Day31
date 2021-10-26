603ba6e560d940edf8f049227eafbac1a57ed51c
Snehal Patil <sneha.swapnil1996@gmail.com>
2021-10-26 11:48:16 +0530

    Retrieve data using new table

---
+++ UC12.sql
@@ -0,0 +1,26 @@
+/*UC--4*/
+select * from employee e inner join company c on 
+e.company_id=c.company_id inner join payroll p on
+p.payroll_id=e.payroll_id inner join EmployeeDepartment ed on
+e.employee_id=ed.employee_id inner join Department d on
+ed.department_id=d.department_id where e.employee_id=1;
+
+select * from employee e inner join company c on 
+e.company_id=c.company_id inner join payroll p on
+p.payroll_id=e.payroll_id inner join EmployeeDepartment ed on
+e.employee_id=ed.employee_id inner join Department d on
+ed.department_id=d.department_id where e.employee_id=2;
+
+/*UC-5*/
+select basic_pay from employee where start_date between CAST('2020-10-2' as date) and GETDATE();
+
+/*UC-7 */
+select sum(basic_pay) from employee where gender='M' group by gender;
+select sum(basic_pay) from employee where gender='F' group by gender;
+select avg(basic_pay) from employee where gender='M' group by gender;
+select avg(basic_pay) from employee where gender='F' group by gender;
+select min(basic_pay) from employee where gender='M' group by gender;
+select min(basic_pay) from employee where gender='F' group by gender;
+select max(basic_pay) from employee where gender='M' group by gender;
+select max(basic_pay) from employee where gender='F' group by gender;
+select count(phone_number) as 'number of gender' from employee group by gender;
